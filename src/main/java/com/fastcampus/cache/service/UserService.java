0085e2519b6ab843772686959a4dd59f1da2e407
smilejakdu <ash982416@gmail.com>
2024-06-22 22:02:35 +0900

    get user with redis

--- src/main/java/com/fastcampus/cache/service/UserService.java
+++ src/main/java/com/fastcampus/cache/service/UserService.java
@@ -3,14 +3,25 @@ package com.fastcampus.cache.service;
 import com.fastcampus.cache.domain.entity.User;
 import com.fastcampus.cache.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final RedisTemplate<String, Object> objectRedisTemplate;
 
-    public User getUser(Long id) {
-        return userRepository.findById(id).orElseThrow();
+    public User getUser(final Long id) {
+        var key = "users:%d".formatted(id);
+        var cachedUser = objectRedisTemplate.opsForValue().get(key);
+        if (cachedUser != null) {
+            return (User) cachedUser;
+        }
+        User user = userRepository.findById(id).orElseThrow();
+        objectRedisTemplate.opsForValue().set(key, user, Duration.ofSeconds(30));
+        return user;
     }
 }
